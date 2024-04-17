31794c9e7a32f99717d322a7ef3c1dbdf39a9230
mahfuj <mahfujahmad424@gmail.com>
2024-04-17 11:53:31 +0600

    mongodb initial setup done

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 // middleware
 app.use(cors());
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("airbnb server is running....");
-});
-
-
-// airbnb-server
-// WIoyCmh1wiL0hVz0
-
-
-
 
 const uri = `mongodb+srv://${process.env.SECRET_NAME}:${process.env.SECRET_KEY}@cluster0.fgokub5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -27,16 +16,35 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
+    const userCollection = client.db("airbnbDb").collection("users");
+    const roomsCollection = client.db("airbnbDb").collection("rooms");
+    const bookingCollection = client.db("airbnbDb").collection("bookings");
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(query, updateDoc, options);
+      console.log(email);
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -44,8 +52,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("airbnb server is running....");
+});
 
-
-app.listen(port,()=>{
-    console.log(`airbnb server is running on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`airbnb server is running on port: ${port}`);
+});
